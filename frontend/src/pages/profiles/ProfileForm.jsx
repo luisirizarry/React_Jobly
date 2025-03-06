3af94fcb1f88a8269c412d2af49eefaa828d5584
Luis Irizarry <lirizarry6178@gmail.com>
2025-03-06 00:14:15 -0500

    skeleton for profile form

--- frontend/src/pages/profiles/ProfileForm.jsx
+++ frontend/src/pages/profiles/ProfileForm.jsx
@@ -1,8 +1,63 @@
 import React from "react";
-import './ProfileForm.css'
+import "./ProfileForm.css";
 
 const ProfileForm = () => {
-  return <h1>Profile Page</h1>;
+  const navigate = useNavigate(); // Initialize the navigation function
+
+  const INITIAL_STATE = {
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const success = await login(formData.username, formData.password);
+      if (success) {
+        navigate("/"); // Navigate only if login succeeds
+      } else {
+        setFormData(INITIAL_STATE);
+      }
+    } catch (err) {
+      console.error("Login failed:", err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter your username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter your password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
+      <button type="submit" className="login-button">
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default ProfileForm;
